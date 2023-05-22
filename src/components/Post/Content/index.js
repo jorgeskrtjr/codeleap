b4d4fe9e4f29a3a4a1fe5b52e27a1225ce9f3941
Jorge jr <jorgesilvajunior10@gmail.com>
2023-05-22 16:25:22 -0300

    add: modal

--- src/components/Post/Content/index.js
+++ src/components/Post/Content/index.js
@@ -1,19 +1,51 @@
 import styles from './Content.module.css'
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
+import Modal from 'react-bootstrap/Modal';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function Content({ issue }) {
-    console.log("=>" + issue)
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div className={styles.container}>
 
             <div className={styles.topo}>
-                
+
                 <h3 className={styles.title}>{issue.title}</h3>
 
                 <div className={styles.containerIcons}>
-                    <AiOutlineEdit className={styles.icon}/>
-                    <AiOutlineDelete className={styles.icon}/>
+
+                    <span onClick={handleShow}>
+                        <AiOutlineEdit className={styles.icon} style={{ cursor: 'pointer' }} />
+                    </span>
+
+                    <span onClick={handleShow}>
+                        <AiOutlineDelete className={styles.icon} style={{ cursor: 'pointer' }} />
+                    </span>
                 </div>
+                
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
             </div>
 
